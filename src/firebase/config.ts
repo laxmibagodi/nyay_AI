0fd14581cdea4bffb798c18cfa6e4b356d72781d
Laxmi Bagodi <laxmibagodi25@gmail.com>
2026-03-21 21:33:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1119728535-1c17e",
+  "appId": "1:92559993816:web:3551646d476b4fa6c3a0ad",
+  "apiKey": "AIzaSyAlkZ4LIE4OjO7y2PlKAvblr2ISH9mBiso",
+  "authDomain": "studio-1119728535-1c17e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "92559993816"
+};
